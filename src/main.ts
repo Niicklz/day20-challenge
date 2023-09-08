76d44a4482fc2db27580591fc9a3e02b72b4bedf
Jose Vasquez <josevasquez8365@gmail.com>
2023-09-08 12:42:37 -0400

    fix

--- src/main.ts
+++ src/main.ts
@@ -4,13 +4,17 @@ button.addEventListener("click", (e)=> {
   const btnTarget = e.target
   if(btnTarget) {
     let offsetLeft = (btnTarget as HTMLButtonElement).offsetLeft
+    let offsetTop = (btnTarget as HTMLButtonElement).offsetTop
     // Para no olvidarme, client hace referencia a la posicion en el cliente y offset a la posicion en el boton
     let x = e.clientX - offsetLeft
-    let y = e.clientY - offsetLeft
+    let y = e.clientY - offsetTop
+    
+    
     
     const ripple = document.createElement("span")
     ripple.style.left =`${x}px`
-    ripple.style.right =`${y}px`
+    ripple.style.top =`${y}px`
+    
     
     button.appendChild(ripple)
 
